36499747b7cd8229a124120c610334839fca7109
Magdalena Idzikowska <midzikowska@students.nic.edu>
2023-12-02 12:29:41 -0800

    Modal on click
    Modal on click is done.
    Contact Page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,24 @@
+// Hamburger Menu
 function toggleMenu() {
   var x = document.getElementById("navLinks");
   x.classList.toggle("show");
 }
+
+// Modal
+document.addEventListener('DOMContentLoaded', function () {
+  var showButton = document.querySelector('.show');
+  var mask = document.querySelector('.mask');
+  var modal = document.querySelector('.modal');
+  var closeButton = document.querySelector('.close');
+
+  showButton.addEventListener('click', function () {
+    mask.style.display = 'block';
+    modal.style.display = 'block';
+  });
+
+  closeButton.addEventListener('click', function () {
+    mask.style.display = 'none';
+    modal.style.display = 'none';
+  });
+});
 
